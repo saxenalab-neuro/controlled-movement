644b57b6b97d49be51a8595d672d3ab12d537b88
Jaxton Willman <jaxtonwillman@gmail.com>
2021-11-09 10:40:29 -0500

    Made pipeline better
    Need to remove testing on other experiment values, waste of time really.

--- HPGM/scripts/compareallsystems.m
+++ HPGM/scripts/compareallsystems.m
@@ -5,45 +5,42 @@ HPGMdir = "/home/jaxtonwillman/Desktop/HPGM/";
 
 savedsystemsdir = HPGMdir + "sysid/Testing/SavedSystems/";
 
+testingdatacount = 5000; % TAG: [HARDCODED]
+orders = [10 11 12 13 14 15]; % TAG: [HARDCODED]
 
-experiments = [500 1000 2500 5000]; % TAG: [HARDCODED]
+systems = cell(numel(orders),1);
 
-for experiment = experiments
+for i = 1:numel(orders)
     % Load all system order data for a particular number set
-    sys2filename = savedsystemsdir + "sys_2_" + num2str(experiment) + ".mat";
-    sys4filename = savedsystemsdir + "sys_4_" + num2str(experiment) + ".mat";
-    sys6filename = savedsystemsdir + "sys_6_" + num2str(experiment) + ".mat";
-    sys8filename = savedsystemsdir + "sys_8_" + num2str(experiment) + ".mat";
-    sys10filename = savedsystemsdir + "sys_10_" + num2str(experiment) + ".mat";
-
-    sys2 = load(sys2filename).sys;
-    sys4 = load(sys4filename).sys;
-    sys6 = load(sys6filename).sys;
-    sys8 = load(sys8filename).sys;
-    sys10 = load(sys10filename).sys;
-
-
-    % Load testing data
-    testingdatafilename = HPGMdir + "sysid/Testing/testing_sysiddata.mat";
-    testingdata = load(testingdatafilename).data;
-    testingdata = getexp(testingdata, 1:experiment); % Get the correct number of experiments
-
-    % Compare against Testing data
-    figurename = num2str(experiment) + " Experiments - Testing";
-    ft = figure('Name', figurename, 'NumberTitle', 'off');
-    [y,fit,ic] = compare(testingdata, sys2, sys4, sys6, sys8, sys10)
-    saveas(ft, strcat(savedsystemsdir, figurename, '.fig')) % Save figure for further use
-
-
-    % Load validation data
-    validationdatafilename = HPGMdir + "sysid/Validation/validation_sysiddata.mat";
-    validationdata = load(validationdatafilename).data;
-
-    % Compare against Validation data
-    figurename = num2str(experiment) + " Experiments - Validation";
-    fv = figure('Name', figurename, 'NumberTitle', 'off');
-    [y,fit,ic] = compare(validationdata, sys2, sys4, sys6, sys8, sys10)
-    saveas(fv, strcat(savedsystemsdir, figurename, '.fig')) % Save figure for further use
+    systems{i} = load(savedsystemsdir + "sys_" + orders(i) + "_" + num2str(testingdatacount) + ".mat").sys;
 end
 
+% Load testing data
+testingdatafilename = HPGMdir + "sysid/Testing/testing_sysiddata.mat";
+testingdata = load(testingdatafilename).data;
+testingdata = getexp(testingdata, 1:testingdatacount); % Get the correct number of experiments
+
+
+% Compare against Testing data
+figurename = "Testing";
+ft = figure('Name', figurename, 'NumberTitle', 'off');
+[~,fit,~] = compare(testingdata, systems{:});
+saveas(ft, strcat(savedsystemsdir, figurename, '.fig')) % Save figure for further use
+
+testingfitfilename = savedsystemsdir + "testing_fit.mat";
+save(testingfitfilename, 'fit');
+
+
+% Load validation data
+validationdatafilename = HPGMdir + "sysid/Validation/validation_sysiddata.mat";
+validationdata = load(validationdatafilename).data;
+
+% Compare against Validation data
+figurename = "Validation";
+fv = figure('Name', figurename, 'NumberTitle', 'off');
+[~,fit,~] = compare(validationdata, systems{:});
+saveas(fv, strcat(savedsystemsdir, figurename, '.fig')) % Save figure for further use
+
+validationfitfilename = savedsystemsdir + "validation_fit.mat";
+save(validationfitfilename, 'fit');
 end
